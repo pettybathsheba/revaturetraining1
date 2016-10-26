b764070495b0725baef4e78135598792fc23b46f
Bathsheba Petty <petty.bathsheba@gmail.com>
2016-10-26 11:42:21 -0400

    commit monster

--- DotnetDB/MonsterDB/Script.PreDeployment1.sql
+++ DotnetDB/MonsterDB/Script.PreDeployment1.sql
@@ -9,6 +9,7 @@
                SELECT * FROM [$(TableName)]                    
 --------------------------------------------------------------------------------------
 */
+
 use master;
 go
 
@@ -20,51 +21,13 @@ if (DB_ID(N'MonsterDB') is not null)
 
     create database MonsterDB;
     go
-
+  
     use MonsterDB;
-    create table Monster.Monster
- (
- MonsterId  int not null identity(1,1),
- GenderId int null,
- TitleId nvarchar(25) null, 
- TypeId int not null,
- Name nvarchar(250) not null,
- PicturePath nvarchar(256) null,
- Active bit not null
- );
+    go 
 
-create table Monster.MonsterType
-(
-    MonsterTypeId int not null identity (1,1) primary key,
-    TypeName nvarchar(250) not null,
-    Active bit not null
-    );
+if (SCHEMA_ID(N'MonsterDB') is not null)
+  begin
+    create schema Monster;
+    end;
+   go
 
-    Create table Monster.Gender
-    (
-    GenderId int not null identity (1,1) primary key,
-    GenderName nvarchar(250) not null,
-    Active bit not null
-    );
-
-    create table Monster.Title
-    (
-    TitleId nvarchar(25) not null identity(1,1) primary key,
-    TitleName nvarchar(250) not null,
-    Active bit not null 
-    );
-    go
-    
-
-    alter table Monster.Monster
-        add constraint pk_monster_monsterid primary key clustered(MonsterId);
-
-    alter table Monster.Monster
-        add constraint fk_monster_genderid foreign key (GenderId) references Monster.Gender (GenderId);
-
-    alter table Monster.Monster
-        add constraint fk_monster_titleid foreign key (TitleId) references Monster.Title (TitleId);
-    alter table Monster.Monster
-        add constraint fk_monster_typeid foreign key (TypeId) references Monster.MonsterType (MonsterTypeId);
-
-go
