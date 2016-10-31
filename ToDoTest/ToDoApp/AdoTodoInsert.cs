4d28a4ed575704ef9143959ffbce832d2c5b923d
Bathsheba Petty <petty.bathsheba@gmail.com>
2016-10-31 10:35:27 -0400

    commit to do app

---
+++ ToDoTest/ToDoApp/AdoTodoInsert.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Models;
+
+namespace ToDoApp
+{
+    class AdoTodoInsert
+    {
+        private String connectionstring = ConfigurationManager.ConnectionStrings["ToDoEntities3"].ConnectionString;
+
+        public bool InsertTodo(Models.TodoTableModel item)
+        {
+            var n = new SqlParameter("item", TodoTableModel.Tasktodo);
+            var query = "insert into Monster.Monster(Name, TypeId, Active) values (@name,1, 1)";
+
+            return InsertData(query, n) == 1;
+
+        }
+
+
+
+        private int InsertData(string query, params SqlParameter[] parameters)
+        {
+            int result;
+
+            using (var connection = new SqlConnection(connectionstring))
+            {
+                var cmd = new SqlCommand(query, connection);
+
+                connection.Open();
+                cmd.Parameters.AddRange(parameters);
+                result = cmd.ExecuteNonQuery();
+            }
+
+            return result;
+
+
+
+        }
+
---
+++ ToDoTest/ToDoApp/Models/TodoTableModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    class TodoTableModel
+    {
+        public int TaskId { get; set; }
+        public string TaskToDo { get; set; }
+        public bool Complete { get; set; }
+
+    }
+}
