e30ef2be1097d38dd5a51bb4858a6b4b378c808e
Bathsheba Petty <petty.bathsheba@gmail.com>
2016-11-01 10:15:09 -0400

    commit all

---
+++ MonsterWeb/MonsterWeb.Logic/TestData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using monsterapp.dataclient;
+
+namespace MonsterWeb.Logic
+{
+public class TestData
+    {
+        private MonsterService data = new MonsterService();
+            public void GetGenders()
+        {
+                    data.GetGenders();
+        }
+
+    }
+}
